1039ff7ef91c1f3f4fa31bf4a972f010ff2e8541
ndpendant <dekairr@hotmail.com>
2017-11-16 12:31:58 -0500

    Update app.R

--- archive/app.R
+++ archive/app.R
@@ -185,7 +185,7 @@ server <- function(input, output,session) {
       test <- db[db$CYP... %in% input$CYP_1 | db$CYP... %in% input$CYP_2,]
     }
     test$Extra <- "hello"
-    test$Extra[test$Database == "Drugbank"] <- paste0("https://www.drugbank.ca/unearth/q?utf8=%E2%9C%93&query=",test$Drug,"&searcher=",test$Enzyme)
+    test$Extra[test$Database == "DrugBank"] <- paste0("https://www.drugbank.ca/unearth/q?utf8=%E2%9C%93&query=",test$Drug,"&searcher=",test$Enzyme)
     test<- data.frame(test)
   })
   
