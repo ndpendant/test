6913b43bd6edae1bc59f580ddabaf5152fd5691d
ndpendant <dekairr@hotmail.com>
2017-11-17 22:02:16 -0500

    Update app.R

--- archive/app.R
+++ archive/app.R
@@ -205,7 +205,7 @@ server <- function(input, output,session) {
     #test2$Extra <- "hello"
     #test2$Extra[test2$Database == "DrugBank"] <- test2[,c(4)]#paste0("https://www.drugbank.ca/drugs/",test$DrugID)
     #test2<- data.frame(test2)
-    #fulldt <- c(" "," "," "," "," "," "," "," ") 
+    fulldt <- NULL#c(" "," "," "," "," "," "," "," ") 
     
     #if(nrow(db)>1)
     #{
