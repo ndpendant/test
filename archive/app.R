6c4f1b9501e974a3246ad44556530cb760870882
ndpendant <dekairr@hotmail.com>
2017-12-13 10:14:07 -0500

    Update app.R

--- archive/app.R
+++ archive/app.R
@@ -199,7 +199,7 @@ server <- function(input, output,session) {
       pt1 <- max(inducer1,substrate1,inhibitor1)
       pt2 <- max(inducer2,substrate2,inhibitor2)
       score <- sqrt(pt1*pt2)
-      out <- paste(i, pt1, pt2,score)
+      out <- paste(i,action1, pt1,action2, pt2,score)
       print(out)
       #appending rows to table
       if(length(action1) < length(action2))
