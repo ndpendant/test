393a2ad588b76c35f522cf612e39b58a68239ae7
ndpendant <dekairr@hotmail.com>
2017-12-22 10:02:01 -0500

    Update app.R

--- archive/app.R
+++ archive/app.R
@@ -478,7 +478,7 @@ server <- function(input, output,session) {
     input$GO
     #check_me()
     
-    a <- data.frame(check_me()$dt)
+    a <- data.frame(unlist(check_me()$dt))
     print("converted dt to a")
     print("type of a")
     print(typeof(a))
