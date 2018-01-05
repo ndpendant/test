9eaab743a9e43282672d375e0c3280abe5401afa
ndpendant <dekairr@hotmail.com>
2018-01-05 11:34:34 -0500

    Update app.R

--- archive/app.R
+++ archive/app.R
@@ -546,7 +546,7 @@ server <- function(input, output,session) {
    newtb <<- check_me()$drugs
    for(i in 1:length(dbnames))
    {
-    newtb$Structure[i] <<- str(dba)
+    newtb$Structure[i] <<- HTML(dba)
    }
      print("new table created")
    #print(newtb)
