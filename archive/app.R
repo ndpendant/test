b5adbac13f1c7cad0e8fa1f0318a789f63d2c012
ndpendant <dekairr@hotmail.com>
2018-01-05 10:48:16 -0500

    Update app.R

--- archive/app.R
+++ archive/app.R
@@ -545,7 +545,7 @@ server <- function(input, output,session) {
 #    print("New modals made!!!")
 #    print(modal_made)       
   })
-  check_me()$drugs <- newtb
+#  check_me()$drugs <- newtb
   print("This is the number of modals made!!!")
   print(modal_made)
   print("This is the type for output$")
