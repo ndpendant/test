726aacd869c73128e73cbf55f5ff197c9d2eac4d
ndpendant <dekairr@hotmail.com>
2018-01-05 11:06:10 -0500

    Update app.R

--- archive/app.R
+++ archive/app.R
@@ -539,7 +539,7 @@ server <- function(input, output,session) {
    dblinks
    print("what happened when assigning (length)??")
    print(length(newpics))
-   print(input$dbnames[i])
+   print(str(reactiveValuesToList(input)))
    newtb <<- NULL
    newtb <<- check_me()$drugs
    for(i in 1:length(dbnames))
@@ -547,7 +547,7 @@ server <- function(input, output,session) {
     newtb$Structure[i] <<- input$dbnames[i]
    }
      print("new table created")
-   print(newtb)
+   #print(newtb)
 #   newpics
 #    print("New modals made!!!")
 #    print(modal_made)       
