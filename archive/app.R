4ac9c0b47991215e28be4a59ce0fe74231469720
ndpendant <dekairr@hotmail.com>
2018-01-06 15:38:09 -0500

    Update app.R

--- archive/app.R
+++ archive/app.R
@@ -409,17 +409,19 @@ server <- function(input, output,session) {
       print("this is k")
       print(k)
       kids <- NULL
-      for(i in 1:nrow(k))
-      {
+      #for(i in 1:nrow(k))
+      #{
         temp <-kegg_info[kegg_info$DrugName == k$Drug[i],]$DrugID
+        print("my temp for kegg")
+        print(temp)
         print(k$Drug[i])
         kids <- c(kids,temp)
         
-      }
+      #}
       print("KEGG KIDS!!")
       print(length(kids))
       print(kids)
-      k$DrugID <- kids
+      #k$DrugID <- kids
       k$Extra <- paste0("http://www.kegg.jp/kegg-bin/search_pathway_text?map=map&keyword=",k$Drug,"&mode=1&viewImage=true")
       k$Database <- paste0("<a href='",k$Extra,"'>KEGG</a>")
       k$Extra2 <- paste0("https://www.kegg.jp/Fig/drug/",kegg_info[kegg_info$DrugName == k$Drug,]$DrugID,"/image.svg")
