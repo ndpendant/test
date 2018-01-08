440447bd71854d12b4ab72a316efe401dbf8e4a1
ndpendant <dekairr@hotmail.com>
2018-01-08 10:25:03 -0500

    Update app.R

--- archive/app.R
+++ archive/app.R
@@ -461,7 +461,11 @@ server <- function(input, output,session) {
       modal_stuff$modal_view <- check_me()$drugs[selectedRow,11]
       modal_stuff$modal_name <- check_me()$drugs[selectedRow,1]
       
+      print("selected Row")
+      print(selectedRow)
+      print("link to row")
       print(modal_stuff$modal_view)
+      print("Drug Name")
       print(modal_stuff$modal_name)
       showModal(mod())
   
