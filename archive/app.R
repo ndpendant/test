e987235ee03c4aa49c78e06136021cec0028646d
ndpendant <dekairr@hotmail.com>
2018-01-08 10:50:40 -0500

    Update app.R

--- archive/app.R
+++ archive/app.R
@@ -89,10 +89,10 @@ ui <- fluidPage(
 
 server <- function(input, output,session) {
    
-  shinyInput <- function(FUN, len, id, ...) {
+  shinyInput <- function(FUN, len, id,rn, ...) {
       inputs <- character(len)
       for (i in seq_len(len)) {
-        inputs[i] <- as.character(FUN(paste0(id, i), ...))
+        inputs[i] <- as.character(FUN(paste0(id, rn[i]), ...))
       }
       inputs
     }
