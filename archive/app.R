97947f8c2d621fde55bc094de94f2f5ae1ef666c
ndpendant <dekairr@hotmail.com>
2018-01-09 09:35:16 -0500

    Update app.R

--- archive/app.R
+++ archive/app.R
@@ -522,7 +522,7 @@ server <- function(input, output,session) {
       print(modal_view)
       print("Drug Name")
       print(modal_name)
-      showModal(modalDialog(title=paste(modal_name),HTML(browseURL(modal_view)),easyClose=TRUE,footer=paste("source:",modal_view))    
+      showModal(modalDialog(title=paste(modal_name),HTML(url.show(modal_view)),easyClose=TRUE,footer=paste("source:",modal_view))    
 )
       modal_name <<- NULL
       modal_view <<- NULL
